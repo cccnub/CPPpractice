a0bc098d1ce0a59fd6eac7d2546914b76268ece7
댕 허 <zest2700@gmail.com>
2022-02-17 16:49:18 +0900

    커밋

---
+++ ConsoleApplication1/006자료형Datatype/006자료형Datatype.cpp
@@ -0,0 +1,23 @@
+﻿// 006자료형Datatype.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	// 언어차원에서 지원되는 자료형을 기본 자료형 이라고 한다.
+	// 기본자료형은 기본 연산을 지원한다.
+	// - + * / 
+	// int = 4바이트를 차지하는 정수형
+	// char = 1바이트를 차지하는 문자형 
+	
+	int PlayerAtt = 2;
+	int MonsterHp = 10;
+	
+	MonsterHp = MonsterHp - PlayerAtt;
+
+	int a = 0; 
+	
+	
+}
+
